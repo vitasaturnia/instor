fc24472e5420d58d65e9b32a9842f6a83e0cb461
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-12 03:52:18 -0800

    boomshakalaka

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,6 @@
 // AuthContext.js
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../context/firebaseContext";
+import { auth } from "./firebaseContext";
 
 const AuthContext = createContext();
 

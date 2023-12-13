ac3c471a8dfbcc049d5527eddc1b2057ed18b071
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-13 04:43:09 -0800

    boomshakalaka

--- netlify/functions/youtubeDownloader.js
+++ netlify/functions/youtubeDownloader.js
@@ -1,7 +1,7 @@
 // functions/download.js
 const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs');
+const fs = require('fs').promises; // Using fs.promises for async file operations
 const path = require('path');
 
 exports.handler = async function (event, context) {
@@ -10,25 +10,14 @@ exports.handler = async function (event, context) {
         const info = await ytdl.getInfo(videoUrl);
         const videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
 
-        const response = {
-            downloadLink: `/downloads/${info.videoDetails.title}.mp3`,
-        };
-
+        const downloadLink = `/downloads/${info.videoDetails.title}.mp3`;
         const outputPath = path.resolve('public/downloads', `${info.videoDetails.title}.mp3`);
 
-        await new Promise((resolve, reject) => {
-            ytdl(videoUrl, { format: videoFormat })
-                .pipe(ffmpeg())
-                .audioCodec('libmp3lame')
-                .audioBitrate(320)
-                .on('end', resolve)
-                .on('error', reject)
-                .pipe(fs.createWriteStream(outputPath));
-        });
+        await downloadAndConvert(videoUrl, videoFormat, outputPath);
 
         return {
             statusCode: 200,
-            body: JSON.stringify(response),
+            body: JSON.stringify({ downloadLink }),
         };
     } catch (error) {
         console.error('Error in download function:', error);
@@ -38,3 +27,29 @@ exports.handler = async function (event, context) {
         };
     }
 };
+
+async function downloadAndConvert(videoUrl, videoFormat, outputPath) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const videoStream = ytdl(videoUrl, { format: videoFormat });
+            const ffmpegCommand = ffmpeg(videoStream)
+                .audioCodec('libmp3lame')
+                .audioBitrate(320)
+                .on('end', resolve)
+                .on('error', reject);
+
+            const fileStream = fs.createWriteStream(outputPath);
+
+            // Pipe video stream to ffmpeg and save to file
+            videoStream.pipe(ffmpegCommand).pipe(fileStream);
+
+            // Wait for the process to complete
+            await new Promise((innerResolve, innerReject) => {
+                fileStream.on('finish', innerResolve);
+                fileStream.on('error', innerReject);
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}

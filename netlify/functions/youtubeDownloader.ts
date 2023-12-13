2fdf810083a7012accdc9db9e507e72695347f68
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-13 02:39:18 -0800

    boomshakalaka

--- netlify/functions/youtubeDownloader.ts
+++ netlify/functions/youtubeDownloader.ts
@@ -1,9 +1,8 @@
-const { Handler } = require('@netlify/functions');
+import { Handler } from '@netlify/functions';
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const mkdirp = require('mkdirp');
 
-const handler = async (event) => {
+const handler: Handler = async (event: any) => {
     try {
         const { videoUrl, outputFormat } = JSON.parse(event.body);
 
@@ -22,35 +21,16 @@ const handler = async (event) => {
         const videoId = info.videoDetails.videoId;
         const outputPath = `./downloads/${videoId}/output.${outputFormat}`;
 
-        // Ensure the directory exists
-        await mkdirp(`./downloads/${videoId}`);
-
         // Use ytdl-core to download video as MP3
-        const stream = ytdl(videoUrl, { filter: 'audioonly' });
-        const fileStream = fs.createWriteStream(outputPath);
+        await ytdl(videoUrl, { filter: 'audioonly' })
+            .pipe(fs.createWriteStream(outputPath));
 
-        return new Promise((resolve, reject) => {
-            stream.pipe(fileStream);
-
-            stream.on('end', () => {
-                resolve({
-                    statusCode: 200,
-                    body: JSON.stringify({
-                        downloadLink: outputPath,
-                    }),
-                });
-            });
-
-            stream.on('error', (error) => {
-                reject({
-                    statusCode: 500,
-                    body: JSON.stringify({
-                        error: 'An error occurred during the download. Please try again.',
-                        details: error.message,
-                    }),
-                });
-            });
-        });
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                downloadLink: outputPath,
+            }),
+        };
     } catch (error) {
         console.error('Error during download:', error);
 
@@ -58,10 +38,10 @@ const handler = async (event) => {
             statusCode: 500,
             body: JSON.stringify({
                 error: 'An error occurred during the download. Please try again.',
-                details: error.message,
+                details: error.message, // Include the error message for more information
             }),
         };
     }
 };
 
-module.exports = { handler };
+export { handler };

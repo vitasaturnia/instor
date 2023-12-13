87de4ff80384f3c3c870b4c5778f77160e0df3cb
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-12 21:05:10 -0800

    boomshakalaka

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.tsx';
-import Navbar from './components/Navbar';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Index from './pages/index';
 import Converter from './pages/ytconverter';
 import Contact from './pages/contact';
 import Login from './pages/login';
 import Register from './pages/register';
+import Layout from './components/Layout';
 
 const App: React.FC = () => {
-    const [darkMode, setDarkMode] = useState(false);
-
-    const toggleDarkMode = () => {
-        setDarkMode((prevDarkMode) => !prevDarkMode);
-    };
-
     return (
-        <Router>
+        <>
             <AuthProvider>
-                <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-                <Routes>
-                    <Route path="/" element={<Index />} />
-                    <Route path="/converter" element={<Converter />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
+                <BrowserRouter>
+                    <Layout>
+                        <Routes>
+                            <Route path="/" element={<Index />} />
+                            <Route path="/converter" element={<Converter />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                        </Routes>
+                    </Layout>
+                </BrowserRouter>
             </AuthProvider>
-        </Router>
+        </>
     );
 };
 

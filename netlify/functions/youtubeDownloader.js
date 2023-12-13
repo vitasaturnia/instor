5a50222894d50da953935c913a8bcd058d038317
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-13 04:32:55 -0800

    boomshakalaka

--- netlify/functions/youtubeDownloader.js
+++ netlify/functions/youtubeDownloader.js
@@ -1,70 +1,39 @@
-import { Handler } from '@netlify/functions';
-import { initializeApp } from 'firebase/app';
-import { getAuth, User } from 'firebase/auth';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import ytdl from 'ytdl-core-discord';
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const path = require('path');
 
-// Initialize Firebase
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const storage = getStorage(app);
-
-const handler: Handler = async (event: any) => {
+exports.handler = async function (event, context) {
     try {
-        const { videoUrl, outputFormat } = JSON.parse(event.body);
+        const { videoUrl } = JSON.parse(event.body);
+        const info = await ytdl.getInfo(videoUrl);
+        const videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
 
-        // Validate YouTube URL
-        if (!ytdl.validateURL(videoUrl)) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    error: 'Invalid YouTube URL',
-                }),
-            };
-        }
+        const response = {
+            downloadLink: `/downloads/${info.videoDetails.title}.mp3`,
+        };
 
-        // Download YouTube video
-        const videoInfo = await ytdl.getBasicInfo(videoUrl);
-        const videoId = videoInfo.videoDetails.videoId;
+        const outputPath = path.resolve('.netlify/functions/downloads', `${info.videoDetails.title}.mp3`);
 
-        // Use ytdl-core-discord to download audio stream
-        const audioStream = ytdl(videoUrl, { quality: 'highestaudio' });
-        const buffer = await ytdl.toBuffer(audioStream);
-
-        // Upload the buffer to Firebase Storage
-        const fileName = outputFormat || 'mp3'; // Default to 'mp3' if outputFormat is undefined
-        const storageRef = ref(storage, `downloads/output.${fileName}`);
-        await uploadBytes(storageRef, buffer);
-
-        // Get the download URL for the file
-        const downloadURL = await getDownloadURL(storageRef);
+        await new Promise((resolve, reject) => {
+            ytdl(videoUrl, { format: videoFormat })
+                .pipe(ffmpeg())
+                .audioCodec('libmp3lame')
+                .audioBitrate(320)
+                .on('end', resolve)
+                .on('error', reject)
+                .pipe(fs.createWriteStream(outputPath));
+        });
 
         return {
             statusCode: 200,
-            body: JSON.stringify({
-                downloadLink: downloadURL,
-            }),
+            body: JSON.stringify(response),
         };
     } catch (error) {
-        console.error('Error during download:', error);
-
+        console.error('Error in download function:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({
-                error: 'An error occurred during the download. Please try again.',
-                details: error.message,
-            }),
+            body: JSON.stringify({ error: 'Internal Server Error' }),
         };
     }
 };
-
-export { handler };

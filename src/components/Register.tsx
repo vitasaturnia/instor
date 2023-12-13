23531fbdc186d46f4b1b4265e4346f7033b82be5
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-12 22:43:19 -0800

    boomshakalaka

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -22,9 +22,7 @@ const Register: React.FC<RegisterProps> = ({ toggleForm }) => {
             setSuccessMessage(null);
         }, 3000);
 
-        return () => {
-            clearTimeout(successMessageTimeout);
-        };
+        return () => clearTimeout(successMessageTimeout);
     }, [successMessage]);
 
     const handleRegister = async (e: FormEvent) => {
@@ -53,69 +51,9 @@ const Register: React.FC<RegisterProps> = ({ toggleForm }) => {
                     <div className="columns is-centered">
                         <div className="column is-5-tablet is-4-desktop is-3-widescreen">
                             <form onSubmit={handleRegister} className="box">
-                                <div className="field">
-                                    <label htmlFor="email" className="label">
-                                        Email
-                                    </label>
-                                    <div className="control">
-                                        <input
-                                            className="input"
-                                            type="email"
-                                            id="email"
-                                            placeholder="Your email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="field">
-                                    <label htmlFor="password" className="label">
-                                        Password
-                                    </label>
-                                    <div className="control">
-                                        <input
-                                            className="input"
-                                            type="password"
-                                            id="password"
-                                            placeholder="Your password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="field">
-                                    <label htmlFor="confirmPassword" className="label">
-                                        Confirm Password
-                                    </label>
-                                    <div className="control">
-                                        <input
-                                            className="input"
-                                            type="password"
-                                            id="confirmPassword"
-                                            placeholder="Confirm your password"
-                                            value={confirmPassword}
-                                            onChange={(e) => setConfirmPassword(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                </div>
-
-                                {error && (
-                                    <div className="notification is-danger">
-                                        <p className="subtitle">{error}</p>
-                                    </div>
-                                )}
-
-                                {successMessage && (
-                                    <div className="notification is-success">
-                                        <p className="subtitle">{successMessage}</p>
-                                    </div>
-                                )}
-
+                                {/* ... */}
+                                {/* Your form fields and error/success messages */}
+                                {/* ... */}
                                 <div className="field">
                                     <div className="control">
                                         <button type="submit" className="button is-primary">
@@ -126,7 +64,10 @@ const Register: React.FC<RegisterProps> = ({ toggleForm }) => {
                             </form>
                             <p>
                                 Already have an account?
-                                <span onClick={toggleForm} style={{ cursor: 'pointer', marginLeft: '5px', color: 'blue' }}>
+                                <span
+                                    onClick={toggleForm}
+                                    style={{ cursor: 'pointer', marginLeft: '5px', color: 'blue' }}
+                                >
                   Login here
                 </span>
                             </p>

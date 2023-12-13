8b13582a97ce1795c56cae511da2919c7f958ded
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-13 03:38:17 -0800

    boomshakalaka

--- src/components/Ytconverter.tsx
+++ src/components/Ytconverter.tsx
@@ -1,100 +1,16 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { useSpring, animated } from 'react-spring';
-import axios from 'axios';
+// components/VideoDownloaderPage.js
+import React from 'react';
+import VideoDownloader from '../components/youtubeDownloader.tsx';
 
-interface DownloadResponse {
-    downloadLink: string;
-}
-
-const YouTubeDownloader: React.FC = () => {
-    const [videoUrl, setVideoUrl] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [downloadLink, setDownloadLink] = useState('');
-    const [outputFormat, setOutputFormat] = useState<'mp3' | 'mp4' | 'aac' | 'ogg' | 'flac' | 'wav'>('mp3');
-    const [error, setError] = useState<string | null>(null);
-
-    const loaderAnimation = useSpring({
-        opacity: loading ? 1 : 0,
-        from: { opacity: 0 },
-        reset: loading,
-    });
-
-    const handleDownload = async () => {
-        try {
-            setLoading(true);
-            setError(null);
-
-            const response = await axios.post<DownloadResponse>('/.netlify/functions/youtubeDownloader', {
-                videoUrl,
-                outputFormat,
-            });
-
-            if (response.status === 200) {
-                setDownloadLink(response.data.downloadLink);
-            } else {
-                setError('An error occurred during the download. Please try again.');
-            }
-        } catch (error) {
-            console.error('Error during download:', error);
-            setError('An error occurred during the download. Please try again.');
-        } finally {
-            setLoading(false);
-        }
-    };
-
+const VideoDownloaderPage = () => {
     return (
-        <div id="youtubeDownloader" className={`box ${outputFormat === 'mp3' ? 'is-light' : ''}`}>
-            <h1>YouTube Downloader</h1>
-            <input
-                type="text"
-                placeholder="Enter YouTube Video URL"
-                value={videoUrl}
-                onChange={(e) => setVideoUrl(e.target.value)}
-            />
-            <button onClick={handleDownload} disabled={loading}>
-                {loading ? (
-                    <FontAwesomeIcon icon={faSpinner} spin />
-                ) : (
-                    'Download'
-                )}
-            </button>
-
-            {error && (
-                <div className="notification is-danger">
-                    {error}
-                </div>
-            )}
-
-            {downloadLink && (
-                <div>
-                    <p className="download-link">
-                        Download Link: <a href={downloadLink} download>Download {outputFormat.toUpperCase()}</a>
-                    </p>
-                </div>
-            )}
-
-            <div className="loader">
-                <animated.div style={loaderAnimation}>&nbsp;</animated.div>
+        <section className="section">
+            <div className="container">
+                <h1 className="title">YouTube Downloader</h1>
+                <VideoDownloader />
             </div>
-
-            <div className="output-options">
-                {['mp3', 'mp4', 'aac', 'ogg', 'flac', 'wav'].map((format) => (
-                    <label key={format}>
-                        <input
-                            type="radio"
-                            name="outputFormat"
-                            value={format}
-                            checked={outputFormat === format}
-                            onChange={() => setOutputFormat(format as typeof outputFormat)}
-                        />
-                        <span>{format.toUpperCase()}</span>
-                    </label>
-                ))}
-            </div>
-        </div>
+        </section>
     );
 };
 
-export default YouTubeDownloader;
+export default VideoDownloaderPage;

475eadb20d7fd91bd8797bbd8d4d16b33bf51977
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-16 20:31:47 -0800

    boomshakalaka

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,12 @@
 import React, { useState, FormEvent, ChangeEvent } from 'react';
-import { useAuth } from '../context/AuthContext.tsx';
-import { useFirebase } from '../context/firebaseContext.tsx';
+import { useAuth } from '../context/AuthContext';
+import { useFirebase } from '../context/firebaseContext';
+import { updateProfile, updatePassword, EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 const Profile = () => {
-    const { user, auth, storage } = useFirebase();
+    const { user } = useAuth();
+    const { auth, storage } = useFirebase();
     const [displayName, setDisplayName] = useState(user?.displayName || '');
     const [newPassword, setNewPassword] = useState('');
     const [currentPassword, setCurrentPassword] = useState('');
@@ -18,57 +20,73 @@ const Profile = () => {
         setSuccess('');
 
         try {
-            if (newPassword) {
-                // Re-authentication is required for password update
+            if (newPassword && user) {
                 const credential = EmailAuthProvider.credential(user.email, currentPassword);
                 await reauthenticateWithCredential(user, credential);
                 await updatePassword(user, newPassword);
             }
 
-            if (profilePic) {
+            let photoURL = user?.photoURL;
+            if (profilePic && user) {
                 const storageRef = ref(storage, `profile-pics/${user.uid}`);
                 await uploadBytes(storageRef, profilePic);
-                const photoURL = await getDownloadURL(storageRef);
-                await updateProfile(user, { displayName, photoURL });
-            } else {
-                await updateProfile(user, { displayName });
+                photoURL = await getDownloadURL(storageRef);
             }
 
+            await updateProfile(user, { displayName, photoURL });
             setSuccess('Profile updated successfully.');
         } catch (error) {
-            setError('Failed to update profile. ' + error.message);
+            setError('Failed to update profile: ' + error.message);
         }
     };
 
     const handleProfilePicChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files && e.target.files[0]) {
-            setProfilePic(e.target.files[0]);
-        }
+        const file = e.target.files?.[0] || null;
+        setProfilePic(file);
     };
 
     return (
-        <div>
-            <h1>Profile</h1>
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-            {success && <div style={{ color: 'green' }}>{success}</div>}
-            <form onSubmit={handleUpdateProfile}>
-                <div>
-                    <label>Display Name</label>
-                    <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
+        <div className="container mt-5">
+            <h1 className="title">Profile</h1>
+            {error && <div className="notification is-danger">{error}</div>}
+            {success && <div className="notification is-success">{success}</div>}
+            <form onSubmit={handleUpdateProfile} className="box">
+                <div className="field">
+                    <label className="label">Display Name</label>
+                    <div className="control">
+                        <input className="input" type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
+                    </div>
                 </div>
-                <div>
-                    <label>New Password</label>
-                    <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+
+                <div className="field">
+                    <label className="label">New Password</label>
+                    <div className="control">
+                        <input className="input" type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+                    </div>
                 </div>
-                <div>
-                    <label>Current Password</label>
-                    <input type="password" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} />
+
+                <div className="field">
+                    <label className="label">Current Password</label>
+                    <div className="control">
+                        <input className="input" type="password" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} />
+                    </div>
                 </div>
-                <div>
-                    <label>Profile Picture</label>
-                    <input type="file" accept="image/*" onChange={handleProfilePicChange} />
+
+                <div className="field">
+                    <label className="label">Profile Picture</label>
+                    <div className="file">
+                        <label className="file-label">
+                            <input className="file-input" type="file" accept="image/*" onChange={handleProfilePicChange} />
+                            <span className="file-cta">
+                                <span className="file-label">Choose a file…</span>
+                            </span>
+                        </label>
+                    </div>
                 </div>
-                <button type="submit">Update Profile</button>
+
+                <div className="control">
+                    <button type="submit" className="button is-primary">Update Profile</button>
+                </div>
             </form>
         </div>
     );
